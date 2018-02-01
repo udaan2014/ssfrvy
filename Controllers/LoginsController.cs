ad0b9e5ada4dbad0869905b4108f81660190196d
udaan2014 <35246456+udaan2014@users.noreply.github.com>
2018-02-01 08:24:09 +0530

    Add files via upload

---
+++ Controllers/LoginsController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginsController : Controller
+    {
+        private NORTHWNDEntities db = new NORTHWNDEntities();
+
+        // GET: Logins
+
+        public ActionResult Login()
+        {
+          
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection formCollectionObject)
+        {
+            string _userID = formCollectionObject[0].ToString();
+            string _password = formCollectionObject[1].ToString();
+            if (formCollectionObject[0].Trim().Length==0)
+            {
+                ViewData["error"] += "Please Enter UserID";
+            }
+            if (formCollectionObject[1].Trim().Length == 0)
+            {
+                ViewData["error"] += "Please Enter Password";
+            }
+            if(formCollectionObject[0].Trim().Length >0 && formCollectionObject[1].Trim().Length>0)
+            {
+                var matchingUser = db.Logins.Where(u => u.UserID ==_userID ).Where(u => u.Password == _password).Select(u => u.UserName).ToList();
+                if(matchingUser.Count==0)
+                {
+                    ViewData["error"] = "Wrong UserID and Password";
+                }
+                else
+                {
+                    Session["userID"] =_userID;                    
+                    return RedirectToAction("Index", "User");
+                }              
+            } 
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+
+
+
+        public ActionResult Index()
+        {
+            return View(db.Logins.ToList());
+        }
+
+        // GET: Logins/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Login login = db.Logins.Find(id);
+            if (login == null)
+            {
+                return HttpNotFound();
+            }
+            return View(login);
+        }
+
+        // GET: Logins/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Logins/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserID,UserName,Password")] Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Logins.Add(login);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(login);
+        }
+
+        // GET: Logins/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Login login = db.Logins.Find(id);
+            if (login == null)
+            {
+                return HttpNotFound();
+            }
+            return View(login);
+        }
+
+        // POST: Logins/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserID,UserName,Password")] Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(login).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(login);
+        }
+
+        // GET: Logins/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Login login = db.Logins.Find(id);
+            if (login == null)
+            {
+                return HttpNotFound();
+            }
+            return View(login);
+        }
+
+        // POST: Logins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Login login = db.Logins.Find(id);
+            db.Logins.Remove(login);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        private NORTHWNDEntities db = new NORTHWNDEntities();
+        // GET: User
+        public ActionResult Index()
+        {
+            if (Session["userID"] != null)
+            {
+                var _userID = Session["userID"].ToString();
+                ViewData["userID"] = _userID;
+                var _userName = db.Logins.Where(u => u.UserID == _userID).Select(u => u.UserName).ToList(); ;
+                ViewData["userName"] = _userName[0];
+                Session["name"] = _userName[0];
+                var categoryChartData = db.Products.GroupBy(p => p.Category.CategoryName).Select(y => new { Name = y.Key, Count = y.Count() });
+                string s = "[";
+                foreach (var item in categoryChartData)
+                {
+                    s += "{" + "label:" + '\"' + item.Name + '\"' + ",y:" + item.Count + "},";
+                }
+                s += "]";
+                ViewBag.categoryChartResult = s;
+
+
+                var supplierChartData = db.Products.GroupBy(p => p.Supplier.CompanyName).Select(y => new { Name = y.Key, Count = y.Count() });
+                string r = "[";
+                foreach (var item in supplierChartData)
+                {
+                    r += "{" + "label:" + '\"' + item.Name + '\"' + ",y:" + item.Count + "},";
+                }
+                r += "]";
+                ViewBag.supplierChartResult = r;
+                return View();
+            }
+            else
+            {
+
+                return RedirectToAction("Login", "Logins");
+            }
+        }
+        public ActionResult Products()
+        {
+            ViewData["deleteFlag"] = 0;
+            ViewData["addFlag"] = 0;
+            if (Session["userID"] == null)
+            {
+                return RedirectToAction("Login", "Logins");
+            }
+            else
+            {
+                var products = db.Products.ToList();
+                ViewBag.products = products;
+                //IEnumerable<Product> product = db.Products.ToList();
+                ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName");
+                ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName");
+                return View();
+            }
+        }
+
+
+        public ActionResult Chart()
+        {
+            if (Session["userID"] == null)
+            {
+                return RedirectToAction("Login", "Logins");
+            }
+            else
+            {
+                var categoryChartData = db.Products.GroupBy(p => p.Category.CategoryName).Select(y => new { Name = y.Key, Count = y.Count() });
+                string s = "[";
+                foreach (var item in categoryChartData)
+                {
+                    s += "{" + "label:" + '\"' + item.Name + '\"' + ",y:" + item.Count + "},";
+                }
+                s += "]";
+                ViewBag.categoryChartResult = s;
+
+
+                var supplierChartData = db.Products.GroupBy(p => p.Supplier.CompanyName).Select(y => new { Name = y.Key, Count = y.Count() });
+                string r = "[";
+                foreach (var item in supplierChartData)
+                {
+                    r += "{" + "label:" + '\"' + item.Name + '\"' + ",y:" + item.Count + "},";
+                }
+                r += "]";
+                ViewBag.supplierChartResult = r;
+
+
+                return View();
+            }
+        }
+
+        public ActionResult DeleteProducts(string productID)
+        {
+            int prodID = int.Parse(productID);
+            Product product = db.Products.Find(prodID);
+            db.Products.Remove(product);
+            if (db.SaveChanges() > 0)
+                return Content("Success");
+            else
+                return Content("Failed");
+        }
+
+        [HttpPost]
+
+        public ActionResult Products([Bind(Include = "ProductID,ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,Discontinued")] Product product, string add, string edit)
+        {
+
+            //if (ModelState.IsValid)
+            if (!string.IsNullOrEmpty(add))
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                ModelState.Clear();
+            }
+            else if (!string.IsNullOrEmpty(edit))
+            {
+                Product productObj = db.Products.Find(product.ProductID);
+                productObj.ProductName = product.ProductName;
+                productObj.SupplierID = product.SupplierID;
+                productObj.CategoryID = product.CategoryID;
+                productObj.QuantityPerUnit = product.QuantityPerUnit;
+                productObj.UnitPrice = product.UnitPrice;
+                productObj.UnitsInStock = product.UnitsInStock;
+                productObj.UnitsOnOrder = product.UnitsOnOrder;
+                productObj.ReorderLevel = product.ReorderLevel;
+                productObj.Discontinued = product.Discontinued;
+                db.SaveChanges();
+
+            }
+            var products = db.Products.ToList();
+            ViewBag.products = products;
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", product.SupplierID);
+
+            return View(product);
+
+        }
+
+
+        public ActionResult MyProfile()
+        {
+            if (Session["userID"] != null)
+            {
+                var _userID = Session["userID"].ToString();
+                ViewData["userID"] = _userID;
+                var _userName = db.Logins.Where(u => u.UserID == _userID).Select(u => u.UserName).ToList(); ;
+                ViewData["userName"] = _userName[0];
+                Session["name"] = _userName[0];
+                return View();
+            }
+
+            else
+            {
+                return RedirectToAction("Login", "Logins");
+            }
+
+        }
+        public ActionResult Logout()
+        {
+            Session.RemoveAll();
+            return RedirectToAction("Login", "Logins");
+        }
+
+    }
+
+}
